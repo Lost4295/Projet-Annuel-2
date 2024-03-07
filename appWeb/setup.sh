d0fea5d5c4fc5ddc1e90d14bc3791d2083961e51
Lost4295 <turin-ylan@outlook.fr>
2024-03-07 17:00:46 +0100

    fix setup.sh

--- appWeb/setup.sh
+++ appWeb/setup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-echo "Now executing git pull"
-git pull
+# echo "Now executing git pull"
+# git pull
 T="composer update"
-myArray=("git pull" "composer update" "mouse" "frog")
+myArray=("git pull" "composer update")
 for i in ${!myArray[@]}; do
-  echo "Now executing $T"
-  echo "element $i is ${myArray[$i]}"
+  echo "Now executing ${myArray[$i]}"
+  echo "---------------------------------------------"
+  ${myArray[$i]}
+  echo "--------------------------------------------"
+# echo "element $i is ${myArray[$i]}"
 done
 # $T  
 sleep 2
