36baf3103c433c56543e9cfb485705baeb6acc5a
Lost4295 <turin-ylan@outlook.fr>
2024-04-09 15:59:26 +0200

    start working on profile interface

--- appWeb/src/Controller/UserController.php
+++ appWeb/src/Controller/UserController.php
@@ -9,8 +9,16 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Validator\Constraints\Type;
 
 class UserController extends AbstractController
 {
@@ -21,35 +29,98 @@ class UserController extends AbstractController
     {
         $user = $this->getUser();
         $form = $this->createFormBuilder($user)
-            ->add('nom', null, [
-                "attr"=>[
-                    "class"=>"form-control",
-                    "disabled"=>"disabled"
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'readonly' => true,
+                ],
+                'constraints' => [
+                    new Email([
+                        'message' => 'The email {{ value }} is not a valid email.',
+                    ]),
+                    new NotBlank([
+                        'message' => 'Please enter an email.',
+                    ]),
+                ],
+                "label" => 'emailfield'
+
+            ])
+            ->add('nom', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'readonly' => true,
+                ],
+                "label" => 'nom',
+
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a name.',
+                    ]),
+                    new Length([
+                        'min' => 3,
+                        'minMessage' => 'Your name should be at least {{ limit }} characters.',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 80,
+                    ]),
+                ],
+            ])
+            ->add('prenom', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'readonly' => true,
+                ],
+                "label" => 'prenom',
+
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a name.',
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'minMessage' => 'Your name should be at least {{ limit }} characters.',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 80,
+                    ]),
+                ],
+                // 
+            ])
+            ->add('birthdate', DateType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'readonly' => true,
+                ],
+                "label" => 'birthdate',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a birthdate.',
+                    ]),
+                    new Type([
+                        'message' => 'Please enter a valid date.',
+                        'type' => 'DateTime',
+                    ])
                 ]
             ])
-            ->add('prenom', null, [
-                "attr"=>[
-                    "class"=>"form-control",
-                    "disabled"=>"disabled"
-                ]
-            ])
-            ->add('birthdate', null, [
-                "attr"=>[
-                    "class"=>"form-control",
-                    "disabled"=>"disabled"
-                ]
-            ])
-            ->add('phoneNumber', null, [
-                "attr"=>[
-                    "class"=>"form-control",
-                    "disabled"=>"disabled"
-                ]
-            ])
-            ->add('email', null, [
-                "attr"=>[
-                    "class"=>"form-control",
-                    "disabled"=>"disabled"
-                ]
+            ->add('phoneNumber', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'readonly' => true,
+                ],
+                "label" => 'phone',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a phone number.',
+                    ]),
+                    new Length([
+                        'min' => 10,
+                        'minMessage' => 'Your phone number should be at least {{ limit }} characters.',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 10,
+                    ]),
+                    new Regex([
+                        'pattern' => '/^[0-9]{10}$/',
+                        'message' => 'Your phone number should contain only numbers.'
+                    ])
+                ],
             ])
             ->getForm()->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
