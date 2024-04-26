7fc9edb59ef134cd4eefe293ee6b4cc5cb3f1a87
Benjamin <aodrenleberre@gmail.com>
2024-04-26 21:16:09 +0200

    ModificationErreurNavAndroid

--- appAndroidV2/app/src/main/java/com/example/appandroidpa/MainActivity.kt
+++ appAndroidV2/app/src/main/java/com/example/appandroidpa/MainActivity.kt
@@ -38,7 +38,8 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_location, R.id.nav_contacts,
+                R.id.nav_presta, R.id.nav_presta
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
