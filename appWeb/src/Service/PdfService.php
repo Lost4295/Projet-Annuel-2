17f429abf8b9c70d2b19698f0907d63638471a6c
Quentin <21quentingoubyollivier@gmail.com>
2024-06-17 00:35:13 +0200

    Facture1 + about + et changement contact

---
+++ appWeb/src/Service/PdfService.php
@@ -0,0 +1,47 @@
+<?php
+// src/Service/PdfService.php
+namespace App\Service;
+
+use Dompdf\Dompdf;
+use Dompdf\Options;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+
+class PdfService
+{
+    private $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    public function generatePdf(array $data): Response
+    {
+        // Configure DomPDF according to your needs
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
+
+        // Instantiate Dompdf with our options
+        $dompdf = new Dompdf($pdfOptions);
+
+        // Retrieve the HTML generated in our twig file
+        $html = $this->twig->render('invoice.html.twig', $data);
+
+        // Load HTML to Dompdf
+        $dompdf->loadHtml($html);
+
+        // (Optional) Setup the paper size and orientation
+        $dompdf->setPaper('A4', 'portrait');
+
+        // Render the HTML as PDF
+        $dompdf->render();
+
+        // Output the generated PDF to Browser (inline view)
+        $response = new Response($dompdf->output());
+        $response->headers->set('Content-Type', 'application/pdf');
+        $response->headers->set('Content-Disposition', 'inline; filename="invoice.pdf"');
+
+        return $response;
+    }
+}
